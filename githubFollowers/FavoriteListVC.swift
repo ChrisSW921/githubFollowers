5c932da9f34d620f2cedc87e628fbecca4827690
Chris <withscottc921@gmail.com>
2021-02-28 11:41:09 -0700

    Initial Commit

---
+++ githubFollowers/FavoriteListVC.swift
@@ -0,0 +1,18 @@
+//
+//  FavoriteListVC.swift
+//  githubFollowers
+//
+//  Created by Chris Withers on 2/27/21.
+//
+
+import UIKit
+
+class FavoriteListVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemBlue
+    }
+
+}
---
+++ githubFollowers/SearchVC.swift
@@ -0,0 +1,18 @@
+//
+//  SearchVC.swift
+//  githubFollowers
+//
+//  Created by Chris Withers on 2/27/21.
+//
+
+import UIKit
+
+class SearchVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemPink
+    }
+
+}
